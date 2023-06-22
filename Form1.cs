0f88a0dc8008490bcac741821410762e9a094b0f
Sugimoto <sugimotodat@gmail.com>
2023-06-22 13:45:36 +0900

    非効率

--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
 
 namespace fukuv_0622
 {
     public partial class Form1 : Form
     {
+        static Random rand = new Random();
+        int a;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] data = new int[10];
+            for (int i = 0; i <= 9; i++)
+            {
+                data[i] = rand.Next(0, 101);
+                a = data[i] + a;
+            }
+            label1.Text = $"{data[0]},{data[1]},{data[2]},{data[3]},{data[4]},{data[5]},{data[6]},{data[7]},{data[8]},{data[9]},";
+
+            a = a / 10;
+            MessageBox.Show($"{a}");
         }
     }
 }
